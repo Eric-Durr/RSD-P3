632db1df03ce013c923101b4167422e56230749f
Eric DS <eric.d.s.info@gmail.com>
2020-05-19 13:00:20 +0100

    Nothing relevant

--- src/ftp_server.cpp
+++ src/ftp_server.cpp
@@ -5,6 +5,7 @@
 
 FTPServer *server;
 
+
 extern "C" void sighandler(int signal, siginfo_t *info, void *ptr) {
   std::cout << "Dispara sigaction" << std::endl;  
   server->stop();
@@ -18,15 +19,19 @@ void exit_handler() {
 
 
 int main(int argc, char **argv) {
+  
 
-  
-  
     struct sigaction action;
+  
     action.sa_sigaction = sighandler;
+    
     action.sa_flags = SA_SIGINFO;
       sigaction(SIGINT, &action , NULL);
+    
     server = new FTPServer(2121);
+    
       atexit(exit_handler);
+    
     server->run();
 
 }
